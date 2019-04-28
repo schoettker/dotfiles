50688e2960045270c08a1c42990eb0457dba064f
LS8 <lennartschoettker@hotmail.com>
2019-04-28 19:57:59 +0200

    Kill redshift before starting it in case its running

--- scripts/.startscript.sh
+++ scripts/.startscript.sh
@@ -1,8 +1,13 @@
 ./.monitor.sh
-#feh --bg-scale ~/dls/wallpaper2.jpg
-xmodmap ~/.xmodmap
-#pkill redshift
+# feh --bg-scale ~/dotfiles/walls/feh_024530_000032_1490826107465.jpg &
+# feh --bg-scale ~/dotfiles/walls/mosaik.jpg &
+feh --bg-scale ~/dotfiles/walls/wallmain.jpg &
+xsetroot -cursor_name left_ptr &
+# setxkbmap -option grp:lctrl_lalt_toggle
+xmodmap ~/.xmodmap &
+pkill redshift
 redshift &
+
 #compton &
 #herbstclient detect_monitors
 #emacs --daemon &

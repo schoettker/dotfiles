5ed0a9da8a915d93dfe27f3d4bbaaddf0584439a
LS8 <lennartschoettker@hotmail.com>
2020-01-30 12:39:10 +0100

    Update monitor script
    - includes (now commented out) command for ultra wide screen

--- scripts/.monitor.sh
+++ scripts/.monitor.sh
@@ -5,6 +5,8 @@ extern=HDMI1
 if xrandr | grep "$extern disconnected"; then
   xrandr --output "$extern" --off --output "$intern" --auto
 else
-  xrandr --output "$intern" --off --output "$extern" --primary --auto
+  # xrandr --output HDMI1 --mode 3440x1440
+  xrandr --output HDMI1 --mode 1920x1080
+  # xrandr --output "$intern" --off --output "$extern" --primary --auto
   #bspc monitor --reset-desktops 1 2 3 4 5 6 7 8 9
 fi

4fc7e9ca19f6e0b5459e79a29623b9485bb99d03
LS8 <lennartschoettker@hotmail.com>
2020-09-08 17:33:13 +0200

    Spawn stuff in the background

--- scripts/.startscript.sh
+++ scripts/.startscript.sh
@@ -1,12 +1,12 @@
 ./.monitor.sh
-picom -b --experimental-backends --dbus --config ~/.config/picom.conf
+picom -b --experimental-backends --dbus --config ~/.config/picom.conf &
 # feh --bg-scale ~/dotfiles/walls/feh_024530_000032_1490826107465.jpg &
 # feh --bg-scale ~/dotfiles/walls/mosaik.jpg &
-feh --bg-scale ~/dotfiles/walls/wallmain.jpg # LE MAIN
+feh --bg-scale ~/dotfiles/walls/wallmain.jpg & # LE MAIN
 
 #feh --bg-scale ~/.config/awesome/theme/yota.jpg &
 xsetroot -cursor_name left_ptr &
-setxkbmap -option grp:lctrl_lalt_toggle
+setxkbmap -option grp:lctrl_lalt_toggle &
 xmodmap ~/.xmodmap &
 # pkill redshift
 # redshift &

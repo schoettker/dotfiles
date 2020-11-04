db4d7888eca22c51370c5cba4707c6dbc4fc634b
LS8 <lennartschoettker@hotmail.com>
2020-11-04 14:23:49 +0100

    add script for + click app launcher on polybar

---
+++ scripts/clicklauncher.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+declare -A actions
+
+actions["left-1"]="google-chrome-stable"
+actions["middle-1"]="brave"
+actions["right-1"]="chromium"
+actions["left-2"]="emacs"
+actions["left-3"]="termite"
+actions["left-4"]="caja"
+actions["right-4"]="termite -e btm"
+
+click=$1
+currentWs=$(($( xdotool get_desktop ) + 1))
+
+${actions["${click}-${currentWs}"]}
+

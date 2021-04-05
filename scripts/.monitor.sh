cd2e0523ed6f67ed9dccb76505552aeb881bde00
LS8 <lennartschoettker@hotmail.com>
2021-04-05 11:30:02 +0200

    update monitor file

--- scripts/.monitor.sh
+++ scripts/.monitor.sh
@@ -2,14 +2,21 @@
 intern=eDP-1
 extern=HDMI-1
 
-# TODO: take the monitor file from home folder, same for x resources
-if xrandr | grep "$extern disconnected"; then
-  xrandr --output "$extern" --off --output "$intern" --mode auto
+# TODO add this to dotfiles
+# if xrandr | grep "$extern disconnected"; then
+  # xrandr --output "$extern" --off --output "$intern" --mode 1920x1080_60.00 --dpi 120
   # xrandr --output "$extern" --off --output "$intern" --mode 1920x1080_60.00 --dpi 96
-else
+# else
   # xrandr --output HDMI1 --mode 3440x1440
-#  xrandr --output HDMI1 --mode 1920x1080
-   # xrandr --output "$intern" --off --output "$extern" --primary --auto
   # xrandr --output HDMI1 --mode 1920x1080
+  # xrandr --output "$intern" --off --output "$extern" --primary --auto
+  #bspc monitor --reset-desktops 1 2 3 4 5 6 7 8 9
+# fi
+if xrandr | grep "$extern disconnected"; then
+  sed -i 's/!//' ~/.Xresources
+  xrandr --output "$extern" --off --output "$intern" --mode 1920x1080_60.00 --dpi 120
+else
+  # xrandr --output HDMI1 --mode 1920x1080
+  sed -i 's/^Xft.dpi:/!Xft.dpi:/' ~/.Xresources
   xrandr --output "$intern" --off --output "$extern" --primary --auto
 fi

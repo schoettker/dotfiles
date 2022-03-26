786d51a3ccff269ecc37687579bb0ffdd130965e
Lennart Schoettker <lennartschoettker@hotmail.com>
2022-03-26 18:41:16 +0100

    add work in progress script to capture current window

---
+++ scripts/screenshot-active-window.sh
@@ -0,0 +1,13 @@
+# #!/usr/bin/env bash
+
+# dir=$HOME/library/images/screenshots
+# if [ ! -d $dir ]; then
+#     mkdir -p $dir
+# fi
+# maim -u -s -m 1 $dir/$(date +%Y%m%d_%H%M%S).png
+
+# maim -i $(xdotool getactivewindow) | xclip -selection clipboard -target image/png
+
+
+# xclip -selection clipboard -t image/png -i $dir/`ls -1 -t $dir | head -1` &
+# # notify-send 'Snap!' 'Selected shot saved and copied to clipboard!'

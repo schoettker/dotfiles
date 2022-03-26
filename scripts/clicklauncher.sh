fc855f251083eea1300e397567cbe753a1393bbe
Lennart Schoettker <lennartschoettker@hotmail.com>
2022-03-26 18:37:16 +0100

    mouse click launch alacritty instead of termite

--- scripts/clicklauncher.sh
+++ scripts/clicklauncher.sh
@@ -5,7 +5,7 @@ actions["left-1"]="google-chrome-stable"
 actions["middle-1"]="brave"
 actions["right-1"]="chromium"
 actions["left-2"]="emacs"
-actions["left-3"]="termite"
+actions["left-3"]="alacritty"
 actions["left-4"]="caja"
 actions["right-4"]="termite -e btm"
 

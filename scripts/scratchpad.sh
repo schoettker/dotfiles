314dc929935a14e36bb67698f2861b367a0194c2
Lennart Schoettker <lschoettker@spotify.com>
2022-10-16 15:40:24 +0200

    Update scratchpad actions to launch emacs with org file and agenda split

--- scripts/scratchpad.sh
+++ scripts/scratchpad.sh
@@ -2,8 +2,8 @@
 
 declare -A actions
 
-actions["scratchterm"]="alacritty --class=scratchterm"
-actions["scratchmacs"]="emacs ~/library/Dropbox/org/scratchpad.org --name scratchmacs"
+actions["scratchterm"]="alacritty --class=scratchterm --title=scratchterm"
+actions["scratchmacs"]="emacs ~/dev/brain/work/spotify.org --name scratchmacs"
 actions["scratchbrowser"]="qutebrowser --qt-arg name scratchbrowser"
 # actions["scratchbrowser"]="firefox --class scratchbrowser -private"
 
@@ -22,6 +22,19 @@ if [ "$id" != "" ]
  then
 	bspc node "$id" -g hidden; bspc node -f "$id"
 else
-	${actions["${action}"]}
+    # extra code to handle emacs `eval` which didnt work otherwise because bash will ignore the needed single quotes eval='(...)'
+    if [ "$action" == "scratchmacs" ]
+      then
+        emacs --name scratchmacs --eval='(progn  (select-window (split-window-below 14)) (find-file "~/dev/brain/work/spotify.org") (previous-window-any-frame) (org-agenda-list) (previous-window-any-frame))'
+    else
+        echo $action
+      ${actions["${action}"]}
+    fi
 fi
 
+
+# actions["scratchmacs"]="emacs ~/dev/brain/work/spotify.org --name scratchmacs --eval='(progn (select-window (split-window-below -20))(org-agenda-list)(previous-window-any-frame))'"
+
+# actions["scratchmacs"]="emacs --name scratchmacs --eval='(progn (find-file \"~/dev/brain/work/spotify.org\") (select-window (split-window-below -20))(org-agenda-list)(previous-window-any-frame))'"
+# actions["scratchmacs"]="emacs --name scratchmacs --eval='(progn  (select-window (split-window-below 14)) (find-file "~/dev/brain/work/spotify.org") (previous-window-any-frame) (org-agenda-list) (previous-window-any-frame))'"
+# actions["scratchmacs"]="emacs --name scratchmacs --eval='(progn (find-file "~/dev/brain/work/spotify.org") (select-window (split-window-below -20))(org-agenda-list)(previous-window-any-frame))'"

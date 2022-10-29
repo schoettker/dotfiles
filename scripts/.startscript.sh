c95f5b7589e2d1afd857d2f0211f6c5f8dba2401
Lennart Schoettker <lschoettker@spotify.com>
2022-10-29 05:44:05 +0200

    update startscript

--- scripts/.startscript.sh
+++ scripts/.startscript.sh
@@ -17,14 +17,15 @@ redshift &
 #syncthing -no-browser &
 flameshot &
 dunst &
+fusuma -d &
 clipster -d &
 bitwarden-desktop &
 # caffeine & -> as caffeine pkgs don't wanna work anymore, just install xfce4-power-manager and configure never sleep in there and then auto-start
+xfce4-power-manager &
 gpclient &
 slack &
 # google-chrome-stable --app="https://calendar.google.com/calendar/render#main_7" &
 thunderbird &
-# fusuma &|
 # dropbox &
 # logseq &
 # emacs --name orgmacs --eval='(progn (org-agenda-list)(split-window-right)(org-capture nil "aj"))' &

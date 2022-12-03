9b03916b34958bddd171e34f3ec4563f68925c6d
Lennart Schoettker <lschoettker@spotify.com>
2022-12-03 23:26:45 +0100

    change wallpaper

--- scripts/.startscript.sh
+++ scripts/.startscript.sh
@@ -2,7 +2,7 @@
 # picom -b --experimental-backends --dbus --config ~/.config/picom.conf &
 # feh --bg-scale ~/dotfiles/walls/feh_024530_000032_1490826107465.jpg &
 # feh --bg-scale ~/dotfiles/walls/mosaik.jpg &
-feh --bg-scale ~/dotfiles/walls/wallAppleYosemite.jpg
+feh --bg-scale ~/dotfiles/walls/mojave-hybdrid-wallpaperflare.jpg &
 
 #feh --bg-scale ~/.config/awesome/theme/yota.jpg &
 xsetroot -cursor_name left_ptr &

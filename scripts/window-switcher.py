2ae4111f029cda7c88ae74bb80bd51139b6fe427
Lennart Schoettker <lschoettker@spotify.com>
2022-12-03 23:27:28 +0100

    window switcher: add high dpi to rofi

--- scripts/window-switcher.py
+++ scripts/window-switcher.py
@@ -6,7 +6,9 @@ stream = os.popen('wmctrl -l')
 windows = stream.read()
 windows = windows.splitlines()
 
+
 stream = os.popen('echo -e "%s" | rofi -theme ~/dotfiles/config/rofi/theme.rasi -dmenu -i -format i -p "window:"' % ('\n'.join(windows)))
+# stream = os.popen('echo -e "%s" | rofi -dpi 192 -theme ~/dotfiles/config/rofi/theme.rasi -dmenu -i -format i -p "window:"' % ('\n'.join(windows)))
 windowIdx = stream.read()
 windowId = windows[int(windowIdx)].split(' ', 1)[0]
 
